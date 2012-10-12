707f6d5c5f68b9bbb9f63a6a7c129579de561b43
yuletide <adyule@gmail.com>
2012-10-12 15:34:37 -0700

    rewrote Projector to use proj4js, but only when in and out projections are passed in, otherwise use old weird math for now

--- projector.js
+++ projector.js
@@ -1,17 +1,54 @@
-// TODO: this should really be more abstract - managing re-projection between any two SRSs
-// TODO: should probably not modify in-place, either
+// TODO this should support passing in projection strings in many formats, including preconstructed Proj4 objects 
+
+var Proj4 = require('proj4js');
+Proj4.defs["EPSG:3857"] = "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +units=m +no_defs";
+Proj4.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+
 var project = {
-    'FeatureCollection': function(fc) { fc.features.forEach(project.Feature); },
-    'Feature': function(f) { project[f.geometry.type](f.geometry.coordinates); },
-    'MultiPolygon': function(mp) { mp.forEach(project.Polygon); },    
-    'Polygon': function(p) { p.forEach(project.LineString); },
-    'MultiLineString': function(ml) { ml.forEach(project.LineString); },
-    'LineString': function(l) { l.forEach(project.Point); },
-    'MultiPoint': function(mp) { mp.forEach(project.Point); },    
-    'Point': function(c) {
-        c[0] = 256.0 * (c[0] + 180) / 360.0;
-        c[1] = 256.0 - 256.0 * (Math.PI + Math.log(Math.tan(Math.PI/4+c[1]*(Math.PI/180)/2))) / (2*Math.PI);
+  'FeatureCollection': function(inProjection, outProjection, fc) { 
+    fc.features.forEach(project.Feature.bind(null, inProjection, outProjection));
+    return fc;
+  },
+  'Feature': function(inProjection, outProjection, f) { 
+    project[f.geometry.type](inProjection, outProjection, f.geometry.coordinates); 
+    return f;
+  },
+  'MultiPolygon': function(inProjection, outProjection, mp) { 
+    mp.forEach(project.Polygon.bind(null, inProjection, outProjection)); 
+    return mp;
+  },
+  'Polygon': function(inProjection, outProjection, p) { 
+    p.forEach(project.LineString.bind(null, inProjection, outProjection));
+    return p;
+  },
+  'MultiLineString': function(inProjection, outProjection, ml) {
+    ml.forEach(project.LineString.bind(null, inProjection, outProjection)); 
+    return ml;
+  },
+  'LineString': function(inProjection, outProjection, l) { 
+    l.forEach(project.Point.bind(null, inProjection, outProjection)); 
+    return l;
+  },
+  'MultiPoint': function(inProjection, outProjection, mp) { 
+    mp.forEach(project.Point.bind(null, inProjection, outProjection)); 
+    return mp;
+  },    
+  'Point': function(inProjection, outProjection, c) {
+    if (inProjection && outProjection) {
+      // TODO: only do this if typeof inProj or outProj is string. Or do it once earlier in the process.
+      var inProj = new Proj4.Proj(inProjection);
+      var outProj = new Proj4.Proj(outProjection);
+      var point = new Proj4.Point(c);
+      //console.log("original coordinate: "+c);
+      Proj4.transform(inProj, outProj, point);
+      c[0] = point.x;
+      c[1] = point.y;
+    } else {
+      c[0] = 256.0 * (c[0] + 180) / 360.0;
+      c[1] = 256.0 - 256.0 * (Math.PI + Math.log(Math.tan(Math.PI/4+c[1]*(Math.PI/180)/2))) / (2*Math.PI);
     }
+    return c;
+  }
 };
 
-exports.project = project;
+module.exports.project = project;
