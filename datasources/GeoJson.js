97e8572746871f5d0d656809f8b45e57ff11e5c0
yuletide <adyule@gmail.com>
2012-10-12 15:37:12 -0700

    Fixes to only project if no load error, project with correct signature,  and not to set default projection for dataset to allow current testing setup to work until internals play nice with projector

--- datasources/GeoJson.js
+++ datasources/GeoJson.js
@@ -3,7 +3,7 @@ var projector = require("../projector");
 
 
 var GeoJsonSource = function(options) {
-  this._projection = options.projection || "4326"; //"+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs";
+  this._projection = options.projection;// || "EPSG:4326"; //"+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs";
   this._path = options.path; // required
   this._encoding = options.encoding || "utf8";
 
@@ -25,8 +25,12 @@ GeoJsonSource.prototype = {
     if (this._projectedData[mapProjection]){
       callback(null, this._projectedData[mapProjection]);
     } else {
-      this.load(function(err, data){
-        this._project(mapProjection);
+      this.load(function(error, data){
+        if (!error){
+            this._project(mapProjection);
+        } else {
+          this._loadError = error;
+        }
         callback(this._loadError, this._projectedData[mapProjection])
       }.bind(this));
     }
@@ -73,7 +77,7 @@ GeoJsonSource.prototype = {
       console.log("Projecting features...");
       start = Date.now();
 
-      this._projectedData[mapProjection] = projector.project.FeatureCollection(this._data, this._projection, mapProjection);
+      this._projectedData[mapProjection] = projector.project.FeatureCollection(this._projection, mapProjection, this._data);
 
       console.log("Projected in " + (Date.now() - start) + "ms"); 
     } else {
