fc8237f4db716b454c192b10a74c54aea5700304
Rob Brackett <rob@robbrackett.com>
2012-10-18 22:08:19 -0700

    Fix bug in projector with default projection

--- datasources/GeoJson.js
+++ datasources/GeoJson.js
@@ -4,7 +4,7 @@ var projector = require(__dirname + "/../lib/projector");
 var FILTER_BY_EXTENTS = true;
 
 var GeoJsonSource = function(options) {
-  this._projection = projector.util.cleanProjString(options.projection) || "EPSG:4326"; //"+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs";
+  this._projection = projector.util.cleanProjString(options.projection || "EPSG:4326");
   this._path = options.path; // required
   this._encoding = options.encoding || "utf8";
   this.name = options.name || options.path.slice(options.path.lastIndexOf("/") + 1);
