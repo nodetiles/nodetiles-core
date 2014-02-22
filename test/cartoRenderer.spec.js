c7f68b0d8d980d5bba9e16c53df0ef6a2f276216
Rob Brackett <rob@robbrackett.com>
2014-02-21 17:32:54 -0800

    Make render test generalizable.

--- test/cartoRenderer.spec.js
+++ test/cartoRenderer.spec.js
@@ -10,83 +10,58 @@ var projector = nodetiles.projector;
 
 describe('cartoRenderer', function() {
   
-  it('should draw markers', function(done) {
-    var style = '\
-      Map {background-color: rgba(122, 122, 122, 1);}\
-      #data {\
-        marker-fill: #ff530d;\
-        marker-line-color: #fff;\
-        marker-width: 8;\
-        marker-line-width: 2;\
-        marker-allow-overlap: true;\
-      }';
-    var data = {
-      "type": "FeatureCollection",                                
-      "features": [
-        { "type": "Feature", "properties": { "DC_NUM": "014848", "DC_KEY": "201001014848", "LOCATION_B": "2100 BLOCK S BROAD ST", "THEFT_DATE": "2010\/04\/16", "THEFT_YEAR": 2010, "DC_DIST": 1, "STOLEN_VAL": 150, "THEFT_HOUR": 0, "UCR": 625, "LAT": 39.9242733, "LNG": -75.169775454 }, "geometry": { "type": "Point", "coordinates": [ -75.169775453999989, 39.924273321000044 ] } },
-        { "type": "Feature", "properties": { "DC_NUM": "016330", "DC_KEY": "201001016330", "LOCATION_B": "S 2100 MCKEAN ST", "THEFT_DATE": "2010\/04\/24", "THEFT_YEAR": 2010, "DC_DIST": 1, "STOLEN_VAL": 215, "THEFT_HOUR": 17, "UCR": 615, "LAT": 39.9270745, "LNG": -75.180905401 }, "geometry": { "type": "Point", "coordinates": [ -75.180905400999961, 39.927074527000059 ] } },
-        { "type": "Feature", "properties": { "DC_NUM": "023568", "DC_KEY": "201001023568", "LOCATION_B": "2700 BLOCK SNYDER AVE", "THEFT_DATE": "2010\/06\/06", "THEFT_YEAR": 2010, "DC_DIST": 1, "STOLEN_VAL": 120, "THEFT_HOUR": 11, "UCR": 625, "LAT": 39.9271197, "LNG": -75.191034151 }, "geometry": { "type": "Point", "coordinates": [ -75.19103415099994, 39.927119746000074 ] } },
-        { "type": "Feature", "properties": { "DC_NUM": "028556", "DC_KEY": "201001028556", "LOCATION_B": "2100 BLOCK S GARNET ST", "THEFT_DATE": "2010\/07\/08", "THEFT_YEAR": 2010, "DC_DIST": 1, "STOLEN_VAL": 200, "THEFT_HOUR": 15, "UCR": 615, "LAT": 39.9254113, "LNG": -75.178257356 }, "geometry": { "type": "Point", "coordinates": [ -75.178257355999961, 39.925411335000035 ] } },
-        { "type": "Feature", "properties": { "DC_NUM": "029047", "DC_KEY": "201001029047", "LOCATION_B": "2100 BLOCK S 15TH ST", "THEFT_DATE": "2010\/07\/11", "THEFT_YEAR": 2010, "DC_DIST": 1, "STOLEN_VAL": 75, "THEFT_HOUR": 11, "UCR": 625, "LAT": 39.9241409, "LNG": -75.171456936 }, "geometry": { "type": "Point", "coordinates": [ -75.17145693599997, 39.924140875000035 ] } }
-      ]
-    };
-    var bounds = projector.util.tileToMeters(1192, 1551, 12);
-    
-    var map = new nodetiles.Map();
-    map.addStyle(style);
-    map.addData({
-      sourceName: "data", // for the stylesheet to use
-      getShapes: function(minX, minY, maxX, maxY, mapProjection) {
-        return projector.project[data.type]("EPSG:4326", mapProjection, data);
-      }
-    });
-    
-    console.log("\nbounds:", bounds);
+  simpleTest('markers');
   
-    map.render({
-      bounds: {minX: bounds[0], minY: bounds[1], maxX: bounds[2], maxY: bounds[3]},
-      width: 256,
-      height: 256,
-      zoom: 12,
-      callback: function(err, result) {
-        result.createPNGStream().pipe(fs.createWriteStream(__dirname + '/markers.output.png'));
-        // result.toDataURL('image/png', function(err, str){
-        //   console.log("OUTPUT:", str);
-        // });
-        
-        var expectedImage = new Canvas.Image();
-        expectedImage.src = fs.readFileSync(__dirname + '/markers.expected.png');
-        // expect(imagediff.equal(result, expectedImage, 100)).to.be.true;
-        
-        // reproduce test logic
-        var tmpCanvas = new Canvas(expectedImage.width, expectedImage.height),
-            ctx = tmpCanvas.getContext('2d');
-        ctx.drawImage(expectedImage, 0, 0, expectedImage.width, expectedImage.height);
-        var aData = ctx.getImageData(0, 0, expectedImage.width, expectedImage.height).data;
-        var bData = result.getContext('2d').getImageData(0, 0, expectedImage.width, expectedImage.height).data;
-        var width = 256;
-        var i, j;
-        for (i = 0; i < aData.length; i += 4) {
-          for (j = 0; j < 4; j++) {
-            var index = i + j;
-            if (aData[index] !== bData[index]) {
-              var col = (i / 4) % 256;
-              var row = Math.floor((i / 4) / 256);
-              console.log("Diff at " + col + ", " + row + " channel ", j, "=", Math.abs(aData[index] - bData[index]), "(", aData[index], "vs.", bData[index], ")");
-            }
-          }
-        }
-        // for (var i = aData.length; i--;) {
-        //   if (aData[i] !== bData[i]) {
-        //     console.log("Diff at ", i, "=", Math.abs(aData[i] - bData[i]));
-        //   }
-        // }
-        
-        // real test
-        expect(imagediff.equal(result, tmpCanvas, 20)).to.be.true;
-        done();
+  /**
+   * Runs a simple test of the renderer against supplied data, styles, and
+   * an expected rendered result. Given a name, it will read:
+   *   - cartoRenderer/name.json (data)
+   *   - cartoRenderer/name.mss  (carto stylesheet)
+   *   - cartoRenderer/name.png  (expected result)
+   */
+  function simpleTest(name) {
+    it(name, function(done) {
+      var base = __dirname + '/cartoRenderer/' + name;
+      var style = fs.readFileSync(base + '.mss', 'utf-8');
+      var data = JSON.parse(fs.readFileSync(base + '.json'));
+      var bounds = data.bounds;
+      if (data.tile) {
+        var extents = projector.util.tileToMeters(data.tile.x, data.tile.y, data.tile.z);
+        bounds = {
+          minX: extents[0],
+          minY: extents[1],
+          maxX: extents[2],
+          maxY: extents[3]
+        };
       }
+      
+      var map = new nodetiles.Map();
+      map.addStyle(style);
+      map.addData({
+        sourceName: "data", // for the stylesheet to use
+        getShapes: function(minX, minY, maxX, maxY, mapProjection) {
+          return projector.project[data.type]("EPSG:4326", mapProjection, data);
+        }
+      });
+  
+      map.render({
+        bounds: bounds,
+        width: 256,
+        height: 256,
+        zoom: data.tile ? data.tile.z : 1,
+        callback: function(err, result) {
+          var expectedImage = new Canvas.Image();
+          expectedImage.src = fs.readFileSync(base + '.png');
+          var expectedCanvas = new Canvas(expectedImage.width, expectedImage.height);
+          var ctx = expectedCanvas.getContext('2d');
+          ctx.drawImage(expectedImage, 0, 0, expectedImage.width, expectedImage.height);
+        
+          // real test
+          expect(imagediff.equal(result, expectedCanvas, 20)).to.be.true;
+          done();
+        }
+      });
     });
-  });
+  }
   
 });
